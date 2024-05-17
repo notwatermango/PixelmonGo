f8f8d351e5322126e246509a5c6bd6362c30201a
danzelmba15 <notwatermango@gmail.com>
2024-05-17 18:25:07 +0700

    Can get lang lat

--- PixelmonGo Watch App/FindView.swift
+++ PixelmonGo Watch App/FindView.swift
@@ -7,34 +7,37 @@
 
 import Foundation
 import SwiftUI
+import CoreLocationUI
+
 
 struct Marker: Hashable {
     let degrees: Double
     let label: String
-
+    
     init(degrees: Double, label: String = "") {
         self.degrees = degrees
         self.label = label
     }
-
+    
     func degreeText() -> String {
         return String(format: "%.0f", self.degrees)
     }
-
-    static func markers() -> [Marker] {
+    
+    static func markers(pixelmonDegree: Double) -> [Marker] {
         return [
-            Marker(degrees: 0, label: "N"),
+            Marker(degrees: 0),
             Marker(degrees: 30),
             Marker(degrees: 60),
-            Marker(degrees: 90, label: "E"),
+            Marker(degrees: 90),
             Marker(degrees: 120),
             Marker(degrees: 150),
-            Marker(degrees: 180, label: "S"),
+            Marker(degrees: 180),
             Marker(degrees: 210),
             Marker(degrees: 240),
-            Marker(degrees: 270, label: "W"),
+            Marker(degrees: 270),
             Marker(degrees: 300),
-            Marker(degrees: 330)
+            Marker(degrees: 330),
+            Marker(degrees: pixelmonDegree, label: "P")
         ]
     }
 }
@@ -42,17 +45,17 @@ struct Marker: Hashable {
 struct CompassMarkerView: View {
     let marker: Marker
     let compassDegress: Double
-
+    
     var body: some View {
         VStack {
-//            Text(marker.degreeText())
-//                .fontWeight(.light)
-//                .rotationEffect(self.textAngle())
-            
+            //                        Text(marker.degreeText())
+            //                            .fontWeight(.light)
+            //                            .rotationEffect(self.textAngle())
             Capsule()
                 .frame(width: self.capsuleWidth(),
                        height: self.capsuleHeight())
                 .foregroundColor(self.capsuleColor())
+                .padding(.bottom, 0)
             
             Text(marker.label)
                 .fontWeight(.bold)
@@ -62,17 +65,17 @@ struct CompassMarkerView: View {
     }
     
     private func capsuleWidth() -> CGFloat {
-        return self.marker.degrees == 0 ? 3 : 1
+        return self.marker.label == "P" ? 3 : 1
     }
-
+    
     private func capsuleHeight() -> CGFloat {
-        return self.marker.degrees == 0 ? 30 : 20
+        return self.marker.label == "P" ? 30 : self.marker.degrees.truncatingRemainder(dividingBy: 90) == 0 ? 20: 10
     }
-
+    
     private func capsuleColor() -> Color {
-        return self.marker.degrees == 0 ? .red : .gray
+        return self.marker.label == "P" ? .red : .gray
     }
-
+    
     private func textAngle() -> Angle {
         return Angle(degrees: -self.compassDegress - self.marker.degrees)
     }
@@ -80,37 +83,49 @@ struct CompassMarkerView: View {
 
 struct FindView: View {
     @ObservedObject var compassHeading = CompassHeading()
+    @State var pixelmonDegree = 40
+    @ObservedObject var locationManager = LocationManager()
+
     var body: some View {
         VStack {
-                    Capsule()
-                        .frame(width: 2,
-                               height: 30)
-
-                    ZStack {
-                        ForEach(Marker.markers(), id: \.self) { marker in
-                            CompassMarkerView(marker: marker,
-                                              compassDegress: self.compassHeading.degrees)
-                        }
-                    }
-                    .frame(width: 150,
-                           height: 150)
-                    .rotationEffect(Angle(degrees: self.compassHeading.degrees))
+            Capsule()
+                .frame(width: 2,
+                       height: 30)
+            ZStack {
+                ForEach(Marker.markers(pixelmonDegree: Double(pixelmonDegree)), id: \.self) { marker in
+                    CompassMarkerView(marker: marker,
+                                      compassDegress: self.compassHeading.degrees)
                 }
-//        NavigationStack {
-//            Text("No nearby pixelmons. Use radar to get direction to a pixelmon.")
-//                .font(.footnote)
-//                .toolbar {
-//                    ToolbarItemGroup(placement: .bottomBar) {
-//                        Button {
-//                            // Click to show temporary radar
-//                        } label: {
-//                            Image(systemName:"location.magnifyingglass")
-//                        }
-//                        .controlSize(.large)
-//                        .background(in: Capsule())
-//                    }
-//                }
-//        }
+            }
+            .frame(width: 150,
+                   height: 150)
+            .rotationEffect(Angle(degrees: self.compassHeading.degrees))
+            if let myLocation = locationManager.location {
+                Text("Latitude: \(myLocation.latitude.formatted(.number.precision(.fractionLength(0)))), Longitude: \(myLocation.longitude.formatted(.number.precision(.fractionLength(0))))".uppercased())
+            } else {
+                LocationButton {
+                    locationManager.requestLocation()
+                }
+                .labelStyle(.iconOnly)
+                .cornerRadius(20)
+            }
+        }
+
+        //        NavigationStack {
+        //            Text("No nearby pixelmons. Use radar to get direction to a pixelmon.")
+        //                .font(.footnote)
+        //                .toolbar {
+        //                    ToolbarItemGroup(placement: .bottomBar) {
+        //                        Button {
+        //                            // Click to show temporary radar
+        //                        } label: {
+        //                            Image(systemName:"location.magnifyingglass")
+        //                        }
+        //                        .controlSize(.large)
+        //                        .background(in: Capsule())
+        //                    }
+        //                }
+        //        }
     }
 }
 
---
+++ PixelmonGo Watch App/LocationManager.swift
@@ -0,0 +1,38 @@
+//
+//  LocationManager.swift
+//  PixelmonGo Watch App
+//
+//  Created by mg0 on 17/05/24.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    @Published var degrees: Double = 0
+    @Published var location: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.startUpdatingHeading()
+        manager.requestWhenInUseAuthorization()
+        manager.allowsBackgroundLocationUpdates = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        degrees = newHeading.trueHeading
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+    }
+}
